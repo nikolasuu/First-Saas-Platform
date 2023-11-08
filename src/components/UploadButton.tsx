190e7b785b1e7307ccb3643248330e5dfb32396c
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-11-08 20:34:13 +0100

    Drag and drop PDF

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -4,6 +4,55 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
 
+import Dropzone from "react-dropzone";
+import { Cloud, File } from "lucide-react";
+
+const UploadDropZone = () => {
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFiles) => {
+        console.log(acceptedFiles);
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
+        >
+          <div className="flex items-center justify-center h-full w-full ">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="mb-2 text-sm text-zinc-700">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+              </div>
+
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-items-center">
+                    <File className="h-4 w-4 text-blue-500" />
+                  </div>
+                  <div className="px-3 py-2 h-full text-sm truncate">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Soluta, omnis.
+                  </div>
+                </div>
+              ) : null}
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
+};
+
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -19,7 +68,9 @@ const UploadButton = () => {
       <DialogTrigger onClick={() => setIsOpen(true)} asChild>
         <Button>Upload PDF</Button>
       </DialogTrigger>
-      <DialogContent>example</DialogContent>
+      <DialogContent>
+        <UploadDropZone />
+      </DialogContent>
     </Dialog>
   );
 };
