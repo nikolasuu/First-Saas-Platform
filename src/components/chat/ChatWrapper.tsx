ee8edac3c0b01de715f247ffd7aae6064357846e
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-11-15 17:44:37 +0100

    Create ChatCTX and ChatInput

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -32,10 +32,11 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
             <p className="text-zinc-500 text-sm">This won&apos;t take long.</p>
           </div>
         </div>
+        <ChatInput isDisabled />
       </div>
     );
 
-  if (data?.status === "PROCESSING" && false)
+  if (data?.status === "PROCESSING")
     return (
       <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
         <div className="flex-1 flex justify-center items-center flex-col mb-28">
@@ -47,10 +48,11 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
             </p>
           </div>
         </div>
+        <ChatInput isDisabled />
       </div>
     );
 
-  if (data?.status === "FAILED" || true)
+  if (data?.status === "FAILED")
     return (
       <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
         <div className="flex-1 flex justify-center items-center flex-col mb-28">
