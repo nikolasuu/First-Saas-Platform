94500f39a2b936310ffa845c6c81b06320e1a1af
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-11-25 15:08:16 +0100

    change url to absoluteUrl

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,12 +4,13 @@ import { PropsWithChildren, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
 import { httpBatchLink } from "@trpc/client";
+import { absoluteUrl } from "@/lib/utils";
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      links: [httpBatchLink({ url: "http://localhost:3000/api/trpc" })],
+      links: [httpBatchLink({ url: absoluteUrl("api/trpc") })],
     })
   );
 
